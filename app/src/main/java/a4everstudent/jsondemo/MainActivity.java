bb63f5ebb86115640358d2dfed742a7abcac3620
4EverStudent <mail@4everstudent.com>
2016-10-19 20:50:27 +0100

    Add JSON code.

--- app/src/main/java/a4everstudent/jsondemo/MainActivity.java
+++ app/src/main/java/a4everstudent/jsondemo/MainActivity.java
@@ -1,16 +1,111 @@
 package a4everstudent.jsondemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    
+    String weatherInfo = "";
+    String link = "http://api.openweathermap.org/data/2.5/weather?q=Lisbon&apikey="+BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        //can't update UI here
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result ="";
+            URL url;
+            HttpURLConnection urlConnection;
+
+            try {
+
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while(data != -1){
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+
+                return result;
+            }
+            catch (MalformedURLException e) {
+
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        //we can update UI here
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+
+                JSONObject jsonObject = new JSONObject(result);
+
+                weatherInfo = jsonObject.getString("weather");
+
+                Log.i("Weather content", weatherInfo);
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                for(int i=0; i < arr.length(); i++){
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    Log.i("main", jsonPart.getString("main"));
+                    Log.i("description", jsonPart.getString("description"));
+
+                }
+            }
+
+            catch (JSONException e) {
+
+                e.printStackTrace();
+
+            }
+
+            Log.i("Website content", result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        task.execute(link);
     }
 }
